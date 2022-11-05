bf2b78472513ffc6eb87f0cec61d14581e2934ae
chushi <tingchushi@hotmail.com>
2022-11-05 12:03:48 +0800

    update

--- apps/server/server.js
+++ apps/server/server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 })
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("../client/dist/index.html"));
+  res.sendFile(path.resolve("../client/client/dist/index.html"));
 });
 
 app.get('/api/admin', (req, res) => {
